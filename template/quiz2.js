5c0cea3b9efdb920bc4b6dc79d739fa736d5ede1
wyakpara <alaskaaricwithana@gmail.com>
2017-04-19 17:06:22 -0400

    I am a failure at this quiz

--- template/quiz2.js
+++ template/quiz2.js
@@ -1,39 +1,45 @@
-(function($){
+$("document").ready(function(){
 	// This is where you will write your function for the programming challenge
 	// Do not commit console.log statements
 	// Use ajax to reach the api endpoint
 	// Whether or not you use jQuery, you still have to fix the jQuery errors. Removing jQuery is not fixing the problem.
 
 	$mouseover = $('.mouseover');
-	$click     = $('.clikc');
-	$sub       = $('.submit');
+	$click     = $('.click');
+	$submit       = $('.submit');
+	$titlebutton = $('#titlebutton');
 
 	$mouseover.on('mouseover', function() {
-		$this = $(this);
 		$(this).html('Scrooge McDuck!');
 		$(this).height($(this).height() + 50);
 	});
 
 	$click.click('click', function() {
-		$this.hmtl('Peace Out!')
-		$(this).fadeout(1500);
+		$(this).html('Peace Out!')
+		$(this).fadeOut(1500);
 		return false;
 	});
 
 	$submit.on('submit', function(e) {
 		e.preventDefault();
 		if ($(this).find('input[type="text"]').val() !== '') {
-			$(this).find('input').foreach(function() {
-				$(this).fadeout('slow');
+			$(this).find('input').each(function() {
+				$(this).fadeOut('slow');
 			});
-			$(this).appendwith('<h2>Congratulations! You've entered some text!</h2>');
+			$(this).append("<h2>Congratulations! You've entered some text!</h2>");
 		}
 	});
 
-	$(document).on(ready, function() {
-		setTimeout({
-			$timeout.fadeIn('slow');
-		}, 1000);
+	$titlebutton.on('click', function() {
+		$.ajax({
+			type: "GET",
+			url: "http://www.mattbowytz.com/simple_api.json"
+			$(data).find('quizData')
+		});
 	});
 
-})(jQuery);
+	setTimeout(function() {
+		$('.timeout').fadeOut('slow');
+	}, 1000);
+
+}(jQuery));
